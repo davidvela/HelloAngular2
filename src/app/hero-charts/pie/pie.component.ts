b7bb03de4d0b1b084c978b684063a0be5973c363
david <david.vela.tirado@gmail.com>
2017-01-28 18:12:23 +0100

    D3JS - pie with labels and legend

--- src/app/hero-charts/pie/pie.component.ts
+++ src/app/hero-charts/pie/pie.component.ts
@@ -5,14 +5,19 @@ import * as d3Shape from "d3-shape";
 
 
 //import { Stats } from './shared/data';
-export const Stats: any[] = [
-  {age: "<5", population: 2704659},
-  {age: "5-13", population: 4499890},
-  {age: "14-17", population: 2159981},
-  {age: "18-24", population: 3853788},
-  {age: "25-44", population: 14106543},
-  {age: "45-64", population: 8819342},
-  {age: "≥65", population: 612463}
+const Dimension: any = {
+    label: "age",
+    value: "population"
+};
+
+const chartData: any[] = [
+  {age: ".Net",           population: 40}, //blue dark - .NET
+  {age: "oracle",         population: 19}, // red  oracle db
+  {age: "java",           population: 40}, // green java
+  {age: "jboss",          population: 15}, // purple jboss
+  {age: "SAP",            population: 59}, // light blue sap 
+  {age: "virtualization", population: 32}, // orange virtualization
+  {age: "other",          population: 10}
 ];
 
 @Component({
@@ -23,6 +28,8 @@ export const Stats: any[] = [
 export class PieComponent implements OnInit {
   title: string = 'D3.js with Angular 2!';
   subtitle: string = 'Pie Chart';
+  widthSVG: number = 500; //- doesn't work in the HTML
+  heightSVG: number = 300;
 
   private margin = {top: 20, right: 20, bottom: 30, left: 50};
   private width: number;
@@ -36,19 +43,26 @@ export class PieComponent implements OnInit {
   private svg: any;
 
   constructor() {
-    this.width = 900 - this.margin.left - this.margin.right ;
-    this.height = 500 - this.margin.top - this.margin.bottom;
+    this.width  = this.widthSVG - this.margin.left - this.margin.right ;
+    this.height = this.heightSVG - this.margin.top - this.margin.bottom;
     this.radius = Math.min(this.width, this.height) / 2;
   }
 
   ngOnInit() {
     this.initSvg()
     this.drawPie();
+    this.drawLegend();
   }
 
   private initSvg() {
+    //set colors 
     this.color = d3Scale.scaleOrdinal()
-                        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+                        //.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+                        //      blue          red        green     purple     blue       orange    other - yellow 
+                          .range(["#086EC2", "#890E0E", "#589531", "#752AE5", "#15A4BD", "#ff8c00", "AFB237"]);
+    //this.color =   d3Scale.scaleOrdinal()
+      //                    .range(d3Scale.schemeCategory10);    
+                          //var colorscale = d3Scale.category10();
     this.arc = d3Shape.arc()
                       .outerRadius(this.radius - 10)
                       .innerRadius(0);
@@ -57,7 +71,8 @@ export class PieComponent implements OnInit {
                            .innerRadius(this.radius - 40);
     this.pie = d3Shape.pie()
                       .sort(null)
-                      .value((d: any) => d.population);
+                     // .value((d: any) => d.population);
+                      .value((d: any) => d[ Dimension.value ] );
     this.svg = d3.select("svg")
                  .append("g")
                  .attr("transform", "translate(" + this.width / 2 + "," + this.height / 2 + ")");;
@@ -65,14 +80,55 @@ export class PieComponent implements OnInit {
 
   private drawPie() {
     let g = this.svg.selectAll(".arc")
-                    .data(this.pie(Stats))
+                    .data(this.pie(chartData))
                     .enter().append("g")
                     .attr("class", "arc");
     g.append("path").attr("d", this.arc)
-                    .style("fill", (d: any) => this.color(d.data.age) );
+                    .attr("stroke-width", 2)
+                    .attr("stroke", "white")
+                    .style("fill", (d: any) => this.color(d.data[Dimension.label]) )
+                    ;
     g.append("text").attr("transform", (d: any) => "translate(" + this.labelArc.centroid(d) + ")")
                     .attr("dy", ".35em")
-                    .text((d: any) => d.data.age);
+                    .text((d: any) => d.data[Dimension.value]);
+  }
+
+  private drawLegend() {
+    let legendTitle = this.svg.append("text")
+                              .attr("class",     "title")
+                              .attr('transform', "translate(-" + this.width / 2 + ",-" + this.height / 2 + ")")
+                              .attr("x", 0)
+                              .attr("y", 10)
+                              .attr("font-size", "12px")
+                              .attr("fill", "#404040")
+                              .text("Legend:");
+    //Initiate Legend	
+    let legend = this.svg.append("g")
+                        .attr("class", "legend")
+                        .attr("height", 100)
+                        .attr("width", 200)
+                        .attr('transform', "translate(-" + this.width / 2 + ",-" + ( this.height / 2 - 20 )+ ")")
+                        ;
+    //create the colour squares 
+    legend.selectAll("rect")
+                      .data(chartData).enter()
+                      .append("rect")
+                      .attr("x", 0)
+                      .attr("y", (d: any, i:any) => i * 20 )
+                      .attr("width", 10)
+                      .attr("height", 10)
+                      .style("fill", (d: any) => this.color(d[Dimension.label])  )
+                      ;
+   //add the texts
+   legend.selectAll("text")
+                      .data(chartData).enter()
+                      .append("text")
+                      .attr("x", 20)
+                      .attr("y", (d:any, i:any) => i*20 + 9)
+                      .attr("font-size", "11px")
+                      .attr("fill", "#737373")
+                      .text((d: any) => d[Dimension.label]);           
+                      ;
   }
 
 }
