cd2b94cab5bb37f47cb6bad5596333bd34ad01ee
david <david.vela.tirado@gmail.com>
2017-03-19 01:38:43 +0100

    test chartjs

--- src/app/hero-charts/hero-charjs/hero-charjs.component.ts
+++ src/app/hero-charts/hero-charjs/hero-charjs.component.ts
@@ -10,6 +10,7 @@ export class HeroCharjsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+   // this.paintCanvas();
   }
 
 
@@ -87,5 +88,31 @@ export class HeroCharjsComponent implements OnInit {
         public lineChartLegend:boolean = true;
         public lineChartType:string = 'line';
 
+
+        paintCanvas() {
+       // var ctx = document.getElementById("myChart").getContext('2d');
+
+       /* var myChart = new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels: ["M", "T", "W", "T", "F", "S", "S"],
+            datasets: [{
+              label: 'apples',
+              backgroundColor: "rgba(153,255,51,1)",
+              data: [12, 19, 3, 17, 28, 24, 7],
+            }, {
+              label: 'oranges',
+              backgroundColor: "rgba(255,153,0,1)",
+              data: [30, 29, 5, 5, 20, 3, 10],
+            }]
+          }
+        });*/
+
+        var labels = {
+          "apples": true,
+          "oranges": true
+        };
+
+    }
 
 }
